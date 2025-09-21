a35b8461d3a6313915f04381d4d0087f2e2433c5
Mohammed Fareed <mohdf.fareed@icloud.com>
2025-09-21 03:22:40 -0400

    Add confirmation prompt for clean repository before tagging new version

--- scripts/update.sh
+++ scripts/update.sh
@@ -7,6 +7,12 @@ uv version --bump "$@"
 echo "updating dependencies..."
 uv lock --upgrade
 
+while ! git diff --quiet; do
+    echo
+    echo "clean repo to tag new version"
+    read -r -p "press any key to continue..."
+done
+
 tag_name="$(uv version --short)"
 echo "updating repo to version: $tag_name"
 git tag -f -a "v$tag_name" -m "version $tag_name"
